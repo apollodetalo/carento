8fccd1ea68577dbede9557bea44b615569cbf49f
Apollo Detalo <apollodetalo90@gmail.com>
2023-06-19 23:25:23 +0800

    few updates

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,55 +1,57 @@
-"use client";
-import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+
 import { footerLinks } from "@/constants";
 
-const Footer = () => {
-  return (
-    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
-      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
-        <div className="flex flex-col justify-start items-start gap-6">
-          <Image
-            src="/carento-logo.svg"
-            className="object-contain"
-            alt="logo"
-            width={118}
-            height={18}
-          />
-
-          <p className="text-base text-gray-700">
-            Carento 2023 <br />
-            All rights reserved &copy;
-          </p>
-        </div>
+const Footer = () => (
+  <footer className="footer">
+    <div className="footer__links-container">
+      <div className="footer__rights">
+        <Image
+          src="/carento-logo.svg"
+          alt="logo"
+          width={118}
+          height={18}
+          className="object-contain"
+        />
+        <p className="text-base text-gray-700">
+          Apollo Detalo | 2023 <br />
+        </p>
       </div>
 
       <div className="footer__links">
-        {footerLinks.map((Link) => (
-          <div key={Link.title} className="footer__link">
-            <h3 className="font-bold">{Link.title}</h3>
-            {Link.links.map((item) => (
-              <a key={item.title} href={item.url} className="text-gray-500">
-                {item.title}
-              </a>
-            ))}
+        {footerLinks.map((item) => (
+          <div key={item.title} className="footer__link">
+            <h3 className="font-bold">{item.title}</h3>
+            <div className="flex flex-col gap-5">
+              {item.links.map((link) => (
+                <Link
+                  key={link.title}
+                  href={link.url}
+                  className="text-gray-500"
+                >
+                  {link.title}
+                </Link>
+              ))}
+            </div>
           </div>
         ))}
       </div>
-      <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
-        <p>@2023 Carento. All Rights Reserved</p>
-        <div className="footer__copyrights-link">
-          <Link href="/" className="text-gray-500">
-            Privacy Policy
-          </Link>
-          <Link href="/" className="text-gray-500">
-            Terms of Use
-          </Link>
-        </div>
+    </div>
+
+    <div className="footer__copyrights">
+      <p> @2023 Carento | All rights reserved &copy;</p>
+
+      <div className="footer__copyrights-link">
+        <Link href="/" className="text-gray-500">
+          Privacy & Policy
+        </Link>
+        <Link href="/" className="text-gray-500">
+          Terms & Condition
+        </Link>
       </div>
-    </footer>
-  );
-};
+    </div>
+  </footer>
+);
 
 export default Footer;
-// "a" tag should be Link tag
